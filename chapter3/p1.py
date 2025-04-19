7d05cdc66f104d827549ebdbb69487ba943511c1
JohelCZarco <101434425+joelzarco@users.noreply.github.com>
2025-04-19 14:06:21 -0600

    tensorDatsets created

--- chapter3/p1.py
+++ chapter3/p1.py
@@ -16,3 +16,20 @@ def create_sequences(df, seq_length):
         xs.append(x)
         ys.append(y)
     return np.array(xs), np.array(ys)
+
+
+ #p2
+# from numpy arrays create TensorDatasets
+import torch
+from torch.utils.data import TensorDataset
+
+# Use create_sequences to create inputs and targets
+X_train, y_train = create_sequences(train_data, 96)
+print(X_train.shape, y_train.shape)
+
+# Create TensorDataset
+dataset_train = TensorDataset(
+    torch.from_numpy(X_train).float(),
+    torch.from_numpy(y_train).float(),
+)
+print(len(dataset_train))
