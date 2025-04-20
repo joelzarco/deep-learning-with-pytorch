6082d3520ec07ba8a43dd25240b2fd093330e9c1
JohelCZarco <101434425+joelzarco@users.noreply.github.com>
2025-04-20 15:12:40 -0600

    LSTM and GRU cells

---
+++ chapter3/p3.py
@@ -0,0 +1,44 @@
+#p3
+#LSTM network
+# A more frequently used alternative that ensures a much better handling of long sequences are Long Short-Term Memory cells.
+
+class Net(nn.Module):
+    def __init__(self, input_size):
+        super().__init__()
+        # Define lstm layer
+        self.lstm = nn.LSTM(
+            input_size=1,
+            hidden_size=32,
+            num_layers=2,
+            batch_first=True,
+        )
+        self.fc = nn.Linear(32, 1)
+
+    def forward(self, x):
+        h0 = torch.zeros(2, x.size(0), 32)
+        # Initialize long-term memory
+        c0 = torch.zeros(2, x.size(0), 32)
+        # Pass all inputs to lstm layer
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+#p4
+# define GRU layer
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Define RNN layer
+        self.gru = nn.GRU(
+            input_size=1,
+            hidden_size=32,
+            num_layers=2,
+            batch_first=True,
+        )
+        self.fc = nn.Linear(32, 1)
+
+    def forward(self, x):
+        h0 = torch.zeros(2, x.size(0), 32)
+        out, _ = self.gru(x, h0)  
+        out = self.fc(out[:, -1, :])
+        return out
