84f41c8bba1f8a2236e587fecb084aa981a2bddd
JohelCZarco <101434425+joelzarco@users.noreply.github.com>
2025-05-02 14:50:10 -0600

    Building a U-Net model

---
+++ chapter7-image-segmentation/p4.py
@@ -0,0 +1,25 @@
+#P4
+# Bulding a U-net architecture model from scratch
+# Define the three convolutional blocks in the decoder by passing the appropriate number of input and output channels to each of them
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+
+        self.enc1 = self.conv_block(in_channels, 64)
+        self.enc2 = self.conv_block(64, 128)
+        self.enc3 = self.conv_block(128, 256)
+        self.enc4 = self.conv_block(256, 512)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        
+        # Define the decoder blocks
+        self.dec1 = self.conv_block(512, 256)
+        self.dec2 = self.conv_block(256, 128)
+        self.dec3 = self.conv_block(128, 64)
+
+        self.out = nn.Conv2d(64, out_channels, kernel_size=1)
